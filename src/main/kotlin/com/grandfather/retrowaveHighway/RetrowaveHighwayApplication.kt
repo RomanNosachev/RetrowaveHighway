e1fb8df00a7f88b05fbd712a3d0b9c0337fa6056
RomanNosachev <walther.landmine@gmail.com>
2020-09-19 12:27:55 +0300

    Initial commit

---
+++ src/main/kotlin/com/grandfather/retrowaveHighway/RetrowaveHighwayApplication.kt
@@ -0,0 +1,11 @@
+package com.grandfather.retrowaveHighway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RetrowaveHighwayApplication
+
+fun main(args: Array<String>) {
+	runApplication<RetrowaveHighwayApplication>(*args)
+}
---
+++ src/test/kotlin/com/grandfather/retrowaveHighway/RetrowaveHighwayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grandfather.retrowaveHighway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RetrowaveHighwayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
